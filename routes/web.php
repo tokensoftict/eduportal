9ecffa4471b245649934c501d53f80a742cb41ae
Yusuf Olatunji <yoyforever@gmail.com>
2024-11-27 08:02:16 +0100

    error fix

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('index');
 Route::get('/about-us', [\App\Http\Controllers\HomeController::class, 'about'])->name('about');
-Route::get('/contact-us', [\App\Http\Controllers\HomeController::class, 'about'])->name('contact');
+Route::get('/contact-us', [\App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
